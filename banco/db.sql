c4e4b6f1b573f5216606d6d05542220629a30ff9
HenriqueBFuzaro <henriquebrambillafuzaro2gmail.com>
2026-05-22 13:15:53 -0300

    banco

--- banco/db.sql
+++ banco/db.sql
@@ -14,7 +14,6 @@ CREATE TABLE ItensExternos(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     codigo VARCHAR(10) NOT NULL UNIQUE,
     etiqueta BOOLEAN NOT NULL DEFAULT 0,
-    definicao VARCHAR(100) NOT NULL,
     descricao TEXT
 );
 
@@ -22,7 +21,6 @@ CREATE TABLE Itens(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     codigo VARCHAR(10) NOT NULL UNIQUE,
     etiqueta BOOLEAN NOT NULL DEFAULT 0,
-    definicao VARCHAR(100) NOT NULL,
     descricao TEXT,
     localizacao VARCHAR(10) NOT NULL,
 
@@ -50,16 +48,16 @@ INSERT INTO Usuarios (nome_usuario,email,senha) VALUES
 ("maria","maria@email.com","teste123");
 
 INSERT INTO ItensExternos
-(codigo, etiqueta, definicao, descricao)
+(codigo, etiqueta, descricao)
 VALUES
-("I010",1,"Armazenamento","HD Seagate 1TB"),
-("I011",0,"Eletronico","Fonte ATX 500W"),
-("I012",1,"Equipamento","Projetor Epson HDMI"),
-("I013",0,"Periferico","Webcam Full HD");
+("I010",1,"HD Seagate 1TB"),
+("I011",0,"Fonte ATX 500W"),
+("I012",1,"Projetor Epson HDMI"),
+("I013",0,"Webcam Full HD");
 
 INSERT INTO Itens
-(codigo,etiqueta,definicao,descricao,localizacao)
+(codigo,etiqueta,descricao,localizacao)
 VALUES
-("I001",1,"Periferico","Mouse USB preto","A1"),
-("I002",1,"Periferico","Teclado mecanico","A1"),
-("I003",0,"Equipamento","Monitor 24 polegadas","B1");
+("I001",1,"Mouse USB preto","A1"),
+("I002",1,"Teclado mecanico","A1"),
+("I003",0,"Monitor 24 polegadas","B1");
